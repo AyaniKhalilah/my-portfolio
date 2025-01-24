526266c941c8e179aafa4122c188140d66b8e62b
AyaniKhalilah <AyaniK.Bilal@gmail.com>
2025-01-24 16:50:41 -0500

    adding Ayani's Arcade to 'projects' section

---
+++ notes.js
@@ -0,0 +1,134 @@
+const title = document.getElementById("about");
+console.log(title.textContent);
+
+const skills = ["C++","HTML", "React", "JavaScript"];
+const skillsList = document.querySelector("#skills ul");
+console.log(skillsList)
+
+//manual construction
+const skillItem = document.createElement("li");
+skillItem.textContent = skills[0];
+//skillsList.appendChild(skillItem);
+
+
+// //variables 
+// let userName = "John";
+// const age = 25;
+// userName = "Jane";
+// const skills = ["HTML", "CSS", "JavaScript"];
+
+// //logging (debugging)
+// console.log("hello world");
+// console.log(skills.at(1));
+
+// //operations
+// const sum = 10 + 5;
+// const product = 10 * 2;
+// console.log(sum, product, "hello world");
+
+// //string stuff
+// const firstName = "Jane";
+// const lastName = "Doe";
+// console.log("Hello, " + firstName + " " + lastName); //concatenation
+// console.log("Hello," , firstName , lastName); //concatenation
+// console.log(`Hello, ${firstName} ${lastName}`); //template literals
+
+// //--------------------------functions---------------------------
+
+// // standard function
+
+// function greet(name) {
+//     return `Hello, ${name} !`;
+// }
+// console.log(greet("John"));
+
+// // anonymous function (is only used once)
+
+// const greet1 = function(name) {
+//     return `Hello, ${name} !`;
+// };
+// console.log(greet1("Jane"));
+
+// const test = function(a, b) {
+//     return a + b;
+// }(3,5); //immmediate invocation
+
+// // fat arrow function notation
+
+// const greet2 = (name) => `Hello, ${name} !`; // single line output statement
+// console.log(greet2("Alex"));
+
+// const greet3 = (name) =>  { // bracketed output statement (can have multilpe lines, needs return statement)
+//     return `Hello, ${name} !`
+// };
+// console.log(greet3("Matt"));
+
+// const square = (num) => num * num; 
+// console.log(function(num) {  //immmediate invocation
+//     return num * num;
+// }(5)); 
+
+// console.log(test);
+
+// // Monday 
+
+// const names = ["Joe", "Jane", "John"];
+// const numbers = [20, 99, 1, 23, 3, 4, 5];
+// names.forEach((name) => (console.log(name)));
+// skills.forEach((skill) => {
+//   console.log(skill);
+// });
+// const squares = numbers.map((num) => num * num);
+// console.log(squares);
+// const evenNumbers = numbers.filter((num) => num % 2 === 0);
+// console.log(evenNumbers);
+// const sum2 = numbers.reduce( (acc, num) => acc + num , 1);
+// console.log(sum2);
+// const firstEven = numbers.find((num) => num % 2 === 0);
+// console.log(firstEven);
+// numbers.sort((a,b) => b - a);
+// console.log(numbers);
+// // const aFunc = (acc, num) => acc + num;
+// // const aFunc = (num) => num % 2 === '0';
+// // for (let i = 0; i < names.length; i++) {
+// //     console.log(names[i]);
+// // }
+// const fruits = ["apple", "banana", "cherry"];
+// fruits.forEach((fruit) => console.log(fruit));
+
+const oneSkill = document.querySelector("#skills li");
+console.log(oneSkill);
+
+oneSkill.addEventListener("click",()=>{
+    console.log("hello");
+});
+function log(){
+    console.log("hello");
+}
+
+const mySkills = document.querySelectorAll("#skills li");
+//console.log(mySkills);
+
+mySkills.forEach((skill)=>{
+    skill.addEventListener("mouseover", () => {
+        skill.style.color = "blue";
+    })
+    skill.addEventListener("mouseout", () => {
+        skill.style.color = "";
+    })
+    skill.addEventListener("click", () => {
+        console.log(skill.textContent);
+    })
+});
+
+const projectCards = document.querySelectorAll(".project");
+projectCards.forEach((project) => {
+  project.addEventListener("mouseover", () => {
+    project.querySelector(".description").style.opacity = 1;
+  });
+  project.addEventListener("mouseout", () => {
+    project.querySelector(".description").style.opacity = 0;
+  });
+});
+
+
--- script.js
+++ script.js
@@ -1,97 +1,10 @@
-const title = document.getElementById("about");
-console.log(title.textContent);
+const requesturl = "https://jsonplaceholder.typicode.com/posts";
 
-const skills = ["C++","HTML", "React", "JavaScript"];
-const skillsList = document.querySelector("#skills ul");
-console.log(skillsList)
-
-//manual construction
-const skillItem = document.createElement("li");
-skillItem.textContent = skills[0];
-//skillsList.appendChild(skillItem);
-
-
-// //variables 
-// let userName = "John";
-// const age = 25;
-// userName = "Jane";
-// const skills = ["HTML", "CSS", "JavaScript"];
-
-// //logging (debugging)
-// console.log("hello world");
-// console.log(skills.at(1));
-
-// //operations
-// const sum = 10 + 5;
-// const product = 10 * 2;
-// console.log(sum, product, "hello world");
-
-// //string stuff
-// const firstName = "Jane";
-// const lastName = "Doe";
-// console.log("Hello, " + firstName + " " + lastName); //concatenation
-// console.log("Hello," , firstName , lastName); //concatenation
-// console.log(`Hello, ${firstName} ${lastName}`); //template literals
-
-// //--------------------------functions---------------------------
-
-// // standard function
-
-// function greet(name) {
-//     return `Hello, ${name} !`;
-// }
-// console.log(greet("John"));
-
-// // anonymous function (is only used once)
-
-// const greet1 = function(name) {
-//     return `Hello, ${name} !`;
-// };
-// console.log(greet1("Jane"));
-
-// const test = function(a, b) {
-//     return a + b;
-// }(3,5); //immmediate invocation
-
-// // fat arrow function notation
-
-// const greet2 = (name) => `Hello, ${name} !`; // single line output statement
-// console.log(greet2("Alex"));
-
-// const greet3 = (name) =>  { // bracketed output statement (can have multilpe lines, needs return statement)
-//     return `Hello, ${name} !`
-// };
-// console.log(greet3("Matt"));
-
-// const square = (num) => num * num; 
-// console.log(function(num) {  //immmediate invocation
-//     return num * num;
-// }(5)); 
-
-// console.log(test);
-
-// // Monday 
-
-// const names = ["Joe", "Jane", "John"];
-// const numbers = [20, 99, 1, 23, 3, 4, 5];
-// names.forEach((name) => (console.log(name)));
-// skills.forEach((skill) => {
-//   console.log(skill);
-// });
-// const squares = numbers.map((num) => num * num);
-// console.log(squares);
-// const evenNumbers = numbers.filter((num) => num % 2 === 0);
-// console.log(evenNumbers);
-// const sum2 = numbers.reduce( (acc, num) => acc + num , 1);
-// console.log(sum2);
-// const firstEven = numbers.find((num) => num % 2 === 0);
-// console.log(firstEven);
-// numbers.sort((a,b) => b - a);
-// console.log(numbers);
-// // const aFunc = (acc, num) => acc + num;
-// // const aFunc = (num) => num % 2 === '0';
-// // for (let i = 0; i < names.length; i++) {
-// //     console.log(names[i]);
-// // }
-// const fruits = ["apple", "banana", "cherry"];
-// fruits.forEach((fruit) => console.log(fruit));
+fetch(requesturl)
+    .then((response)=> (response.JSON()))
+    .then((data)=>{
+        data.forEach(post => {
+            console.log(`ID: ${post.id}, Title: ${post.title}`);
+        });
+    })
+    .catch((error)=>console.log("error fetching data:", error));
